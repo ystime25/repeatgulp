9cf9282535dad5ce160ed7d8a2dd3b93c3a06251
ystime25 <ystime25@gmail.com>
2020-12-28 10:18:44 +0900

    Pug Task part One

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,17 @@
 import gulp from "gulp";
+import gulpPug from "gulp-pug";
+
+const routes = {
+    pug: {
+        src: "src/*.pug",
+        dest: "build"
+    }
+};
+
+export const pug = () => 
+    gulp
+        .src(routes.pug.src)
+        .pipe(gulpPug())
+        .pipe(gulp.dest(routes.pug.dest));
+
+export const dev = gulp.series([pug]);
