7693cd21bfcba28aeaf5ea577e9929e5bd115dbf
ystime25 <ystime25@gmail.com>
2020-12-29 10:01:43 +0900

    Webserver & File watching
    live reload and watch does not fully activate on Windows dev env please keep in mind

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,11 @@
 import gulp from "gulp";
 import gulpPug from "gulp-pug";
 import del from "del";
+import connect from "gulp-connect";
 
 const routes = {
     pug: {
+        watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
     }
@@ -13,13 +15,30 @@ const pug = () =>
     gulp
         .src(routes.pug.src)
         .pipe(gulpPug())
-        .pipe(gulp.dest(routes.pug.dest));
+        .pipe(gulp.dest(routes.pug.dest))
+        .pipe(connect.reload());
 
-const clean = () => del(["build"]);
+const clean = () => del(["build/"]);
+
+const webserver = () => {
+    console.log("Server Activation Comfirmed")
+    connect.server({
+        root: "build",
+        livereload: true,
+        port: 8001
+    })
+};
+
+const detectChange = () =>{
+    console.log("Applying Modifications")
+    gulp.watch(routes.pug.watch, pug)
+};
 
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-export const dev = gulp.series([prepare, assets]);
+const postDev = gulp.series([webserver, detectChange]);
+
+export const dev = gulp.series([prepare, assets, postDev]);
 
